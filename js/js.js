d4c5f384538e0f1d93ee52c113cb1a9e0dca41db
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-09-04 19:31:33 +0300

    add 2w

---
+++ js/js.js
@@ -0,0 +1,107 @@
+// // // const name = "Alex";
+// // let age = prompt `Ведите ваш возраст`;
+// // console.log(typeof age);
+// // console.log(age);
+// // console.log(age + 3);
+// // // age = Number(age)
+// // age = +age;
+// // console.log(age + 3);
+// // // console.log(typeof age);
+// // console.log(age);
+// // let message = 'Привет'+' '+ name;
+// // console.log(message);
+// // let newMessage = `my age is ${age}`;
+// // console.log(newMessage);
+// // let answer = confirm (`Вам есть 18?`);
+// // console.log(answer);
+// // console.log(typeof answer);
+// // let newQuestion = `Вы любите js?`;
+// // // let newAnswer = confirm (newQuestion);
+// // console.log(newAnswer);
+// // console.log(typeof newAnswer);
+
+
+// let num = 23;
+// let boolNum = Boolean(num);
+// console.log(boolNum);
+
+// num = 0;
+// boolNum = Boolean(num);
+// console.log(boolNum);
+
+// let str = `Sasha`;
+// let boolStr = Boolean(str);
+// console.log(boolStr);
+
+// str = ``;
+// boolStr = Boolean(str);
+// console.log(boolStr);
+
+// str = ` `;
+// boolStr = Boolean(str);
+// console.log(boolStr);
+
+// let isOpen;
+// console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolUnd = Boolean(isOpen);
+// console.log(boolUnd);
+// console.log(typeof boolUnd);
+
+// isOpen = null;
+// console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolNull = Boolean (isOpen);
+// console.log(boolNull);
+// console.log(typeof boolNull);
+
+// let vova = {
+//   age: 16,
+//   dad: 'deputat'
+// }
+// if(vova.age >=18) {
+//   alert('Prohodi');
+// } else if (vova.dad === 'deputat'){
+//   alert('Prohodi');
+// } else {
+//   alert('Izvini brat');
+// }
+// if(vova.age >= 18 || vova.dad === 'deputat') {
+//   alert('Prohodi');
+// } else {
+//   alert('Izvini brat');
+// }
+// vova.age >= 18 || vova.dad === 'deputat' ? alert('Prohodi') : alert('Izvini brat');
+
+//  i++
+//  i=i+1
+
+//  ++i
+//  1+i=i
+
+//  const myAge = 31;
+//  const bogdanAge = 24;
+// let resoltAge = myAge + bogdanAge;
+// console.log(resoltAge);
+
+// const radius = 10;
+// const PI = 3.14;
+// console.log(PI * Math.pow(radius, 2));
+
+// const name = 'ALex';
+// const age = 31;
+// console.log(`Я ${name} ${age} на 24 буткемпе!`);
+
+
+
+
+
+
+let a = 10;
+a = 20;
+let b = a.append + a;
+console.log(b);
+
+
