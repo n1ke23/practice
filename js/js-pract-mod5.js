7d09fd709998aa37bb771de834aedba920dcbaf6
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-09-16 17:51:49 +0300

    1aplas[pdla[psd

--- js/js-pract-mod5.js
+++ js/js-pract-mod5.js
@@ -186,24 +186,415 @@
 
 // ============================================================================================================
 
-const mango = new User({
-  name: 'Mango',
-  age: 2,
-  followers: 20,
-});
+// const mango = new User({
+//   name: 'Mango',
+//   age: 2,
+//   followers: 20,
+// });
 
-mango.getInfo(); // User Mango is 2 years old and has 20 followers
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
 
-const poly = new User({
-  name: 'Poly',
-  age: 3,
-  followers: 17,
-});
+// const poly = new User({
+//   name: 'Poly',
+//   age: 3,
+//   followers: 17,
+// });
 
-poly.getInfo(); // User Poly is 3 years old and has 17 followers
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+// ================================================================================================================
+
+// class Cats {
+//   constructor(whiskers = "defolt", teeth = "defolt", tail = "defolt", claws = "defolt"){
+//     this.whiskers = whiskers;
+//     this.teeth = teeth;
+//     this.tail = tail;
+//     this.claws = claws;
+
+//   }
+// }
+
+// class Cat extends Cats {
+//   constructor (whiskers, teeth, tail, claws, ears){
+//     super(whiskers, teeth, tail, claws)
+//     this.ears = ears;
+
+//   }
+// }
+
+// =========================================================================================================================
+
+// task-2
+
+
+// class User{
+//   constructor({name, age, followers}){
+//     this.name = name;
+//     this.age = age;
+//     this.followers = followers;
+//   }
+//   getInfo(){
+//     console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+//   }
+// }
+ 
+// const mango = new User({
+//   name: 'Mango',
+//   age: 2,
+//   followers: 20,
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+//   name: 'Poly',
+//   age: 3,
+//   followers: 17,
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+// =======================================================================================================================================
+
+// task 3
+// class Storage{
+//   constructor(array){
+//     this.items = array
+//   }
+//   getItems(){
+//     return this.items;
+//   }
+//   addItem(item){
+//     return this.items.push(item)
+//   }
+//   removeItem(item){
+//     for(let elm of this.items){
+//       if(item === this.items[elm]){
+//         console.log(elm);
+//         return this.items.splice(elm,1)
+//       }
+//     }
+    
+//   }
+// }
+
+// const storage = new Storage([
+//   'Нанитоиды',
+//   'Пролонгер',
+//   'Железные жупи',
+//   'Антигравитатор',
+// ]);
+
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+// ==================================================================
+// task 4
+
+// class StringBuilder{
+//   constructor(value){
+//     this._value = value;
+//   }
+//   get value(){
+//   return this._value;
+//   }
+//   append(str){
+//     this._value += str;
+//     return this
+//   }
+//   prepend(str){
+//     this._value = str + this._value;
+//     return this
+//   }
+//   pad(str){
+//     this._value = str + this._value + str;
+//   return this
+//   }
+// }
 
 
 
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+
+//  console.log(builder.pad('=').prepend('^').append('^'));
+
+// ====================================================================================================
+// task 5
+
+
+// class Car {
+//   static getSpecs({maxSpeed, _price, speed, isOn, distance}){
+    
+//     console.log(`maxSpeed: ${maxSpeed}, price: ${_price}, speed: ${speed}, isOn: ${isOn}, distance: ${distance} `);
+//   }
+  
+//   /*
+//    * Добавь статический метод `getSpecs(car)`,
+//    * который принимает объект-машину как параметр и выводит
+//    * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+//    */
+
+//   /*
+//    * Конструктор получает объект настроек.
+//    *
+//    * Добавь свойства будущеего экземпляра класса:
+//    *  speed - текущая скорость, изначально 0
+//    *  price - цена автомобиля
+//    *  maxSpeed - максимальная скорость
+//    *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+//    *  distance - общий киллометраж, изначально 0
+//    */
+
+//   constructor({maxSpeed = 0, price = 0, speed = 0, isOn = false, distance = 0}) {
+//     this.maxSpeed = maxSpeed;
+//     this._price = price;
+//     this.speed = speed;
+//     this.isOn = isOn;
+//     this.distance = distance;
+//   }
+//   get price(){
+//    return this._price
+    
+//   }
+//   set price(value){
+//    return this._price = value
+//   }
+
+
+
+//   /*
+//    * Добавь геттер и сеттер для свойства price,
+//    * который будет работать с свойством цены автомобиля.
+//    */
+
+//   /*
+//    * Добавь код для того чтобы завести автомобиль
+//    * Записывает в свойство isOn значение true
+//    */
+//   turnOn() {
+//     this.isOn = true
+//   }
+
+//   /*
+//    * Добавь код для того чтобы заглушить автомобиль
+//    * Записывает в свойство isOn значение false,
+//    * и сбрасывает текущую скорость в 0
+//    */
+//   turnOff() {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+
+//   /*
+//    * Добавялет к свойству speed полученное значение,
+//    * при условии что результирующая скорость
+//    * не больше чем значение свойства maxSpeed
+//    */
+//   accelerate(value) {
+//     let newSpeed = this.speed + value;
+//     if(newSpeed < this.maxSpeed) {
+//       this.speed = newSpeed 
+//     } else {
+//       console.log("Скорость превышает максимальое значение");
+//     }
+//   }
+
+//   /*
+//    * Отнимает от свойства speed полученное значение,
+//    * при условии что результирующая скорость не меньше нуля
+//    */
+//   decelerate(value) {
+//     let newSpeed = this.speed - value;
+//     if(newSpeed >= 0) {
+//       this.speed = newSpeed 
+//     } else {
+//       console.log("Ты стоишь на месте");
+//     }
+//   }
+
+//   /*
+//    * Добавляет в поле distance киллометраж (hours * speed),
+//    * но только в том случае если машина заведена!
+//    */
+//   drive(hours) {
+//     if(this.isOn) {
+//       this.distance += hours * this.speed
+//     } else {
+//       console.log("Заведи машину!");
+//     }
+//   }
+// }
+
+// // const car = {
+// //   maxSpeed: 200,
+// //   price: 2000, 
+// //   speed: 60, 
+// //   isOn: false, 
+// //   distance: 100
+// // };
+// // Car.getSpecs(car)
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+// console.log(mustang);
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
+
+
+
+// let fruits = ["bananas", "melon", "apple"]
+
+
+// let [,,a] = fruits
+
+// console.log(pizza);
+// console.log(a);
+
+
+
+// let user ={
+//   name: "Mango",
+//   age: 102,
+//   email: "aa@BhxBrowser.com",
+//   isAdmin: false,
+//   address:{
+//     street: "Almazova",
+//     city: "Kyiv",
+//     home: 10
+//   },
+//   hobbies: ["footbol", "beer", "tv"]
+// }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// let {maxSpeed, price, speed, isOn, distance} = car
+// let {email, age, address:{city}, address:{home}, hobbies:[footbol, beer, tv]} = user
+// console.log(city);
+// console.log(home);
+// console.log(beer);
+
+// ====================================================================================================================================================================
+
+// const specialPunch = [
+//   { name: "fatality", weapons: "sword", damage: 50, price: 3000 },
+//   { name: "babality", weapons: "knife", damage: 10, price: 2000 },
+//   { name: "brutality", weapons: "nunchak", damage: 25, price: 1000 },
+// ];
+// class Hero {
+//   constructor() {
+//     this._name = "";
+//     this.hp = 3;
+//     this.speed = 50;
+//     this.punchForce = 10;
+//     this._weapons = [];
+//     this.specialPunch = "";
+//   }
+//   addHp(value) {
+//     this.hp += value;
+//   }
+//   killHero(value) {
+//     this.hp -= value;
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   set name(newName) {
+//     return (this._name = newName);
+//   }
+//   get weapons() {
+//     return this._weapons;
+//   }
+//   set weapons(weapon) {
+//     if (!this._weapons.includes(weapon)) {
+//       return this._weapons.push(weapon);
+//     } else console.log("Такое оружие уже есть");
+//   }
+//   chooseSpecialPunch(array, value) {
+//     for (const obj of array) {
+//       if (obj.name === value) {
+//         return (this.specialPunch = obj);
+//       }
+//     }
+//   }
+// }
+// const Lui = new Hero();
+// console.log(Lui);
+// Lui.name = "Sasha";
+// console.log(Lui.name);
+// Lui.chooseSpecialPunch(specialPunch, "brutality");
+// console.log(Lui);
+// class SuperHero extends Hero {
+//   constructor() {
+//     super();
+//     this.superSpeed = false;
+//   }
+//   toggleSuperSpeed() {
+//     this.superSpeed ? (this.superSpeed = false) : (this.superSpeed = true);
+//   }
+// }
+// const Valera = new SuperHero();
+// Valera.name = "Nastya";
+// console.log(Valera);
+// Valera.toggleSuperSpeed();
+// Valera.toggleSuperSpeed();
+
+// =====================================================================================================================
+
+
+// class User {
+// 	constructor(name, surname) {
+// 		this._name = name;
+// 		this._surname = surname;
+// 	}
+
+// 	getFullName() {
+// 		return this._name + ' ' + this._surname;
+// 	}
+// }
+// class Student extends User {
+//   constructor(name, surname, year){
+//     super(name, surname)
+//     this.year = year;
+
+//   }
+
+//   gerCourse (currentYear){
+//     return currentYear - this.year;
+//   }
+// }
+// const Lika = new Student("Lika", "Goncharova", 2014)
+
+// console.log(Lika);
+// console.log(Lika.gerCourse(2020));
 
 
 
